cdc22ab3116e12ce3cb688e92596f23f7d41de8f
kindlich <kindlich@gmx.net>
2020-03-21 23:09:24 +0100

    Fixed Event component

--- frontend/my-theaterlist-app/src/app/einzelnes-event/einzelnes-event.component.ts
+++ frontend/my-theaterlist-app/src/app/einzelnes-event/einzelnes-event.component.ts
@@ -1,27 +1,23 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Event} from './Event';
+import {Component, Input, OnInit} from "@angular/core";
+import {Event} from "./Event";
 
 @Component({
-  selector: 'app-einzelnes-event',
-  templateUrl: './einzelnes-event.component.html',
-  styleUrls: ['./einzelnes-event.component.css']
+  selector: "app-einzelnes-event",
+  templateUrl: "./einzelnes-event.component.html",
+  styleUrls: ["./einzelnes-event.component.css"]
 })
 export class EinzelnesEventComponent implements OnInit {
 
   @Input()
   public dasEvent: Event;
+  public isInfoMooreHidden: boolean;
 
-  constructor() { this.isInfoMooreHidden=true;}
+  constructor() {
+    this.isInfoMooreHidden = true;
+  }
 
-<<<<<<< HEAD
-public isInfoMooreHidden : boolean;
-
-public get bildLink() {
-    return this.dasEvent.thumbnail || "TODO: Default bild";
-=======
   public get bildLink() {
-    return this.dasEvent.thumbnail || 'TODO: Default bild';
->>>>>>> 5566ea809e31c96f719e2de879b77c80ca577ec5
+    return this.dasEvent.thumbnail || "TODO: Default bild";
   }
 
   ngOnInit(): void {
