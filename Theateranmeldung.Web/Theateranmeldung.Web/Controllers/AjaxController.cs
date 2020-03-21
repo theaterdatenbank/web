e34be9e2a03c457598f8b2a0020712f19d99e6fa
Florian Scholz <florian90@gmail.com>
2020-03-21 15:09:08 +0100

    Adding current backend

---
+++ Theateranmeldung.Web/Theateranmeldung.Web/Controllers/AjaxController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Theateranmeldung.Web.Models.Data;
+using Theateranmeldung.Web.Services;
+
+namespace Theateranmeldung.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AjaxController : ControllerBase
+    {
+        private readonly IEventRepository _eventRepository;
+        public AjaxController(IEventRepository eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+        [HttpGet("events")]
+        public List<Event> Events()
+        {
+            return _eventRepository.GetEvents();
+        }
+
+        [HttpGet("events_filtered")]
+        public List<Event> EventsFiltered(string wanted, string notwanted)
+        {
+            return _eventRepository.GetEventsFiltered(wanted, notwanted);
+        }
+
+        [HttpGet("createEvents")]
+        public void CreateSamples()
+        {
+            var event1 = new Event()
+            {
+                Approved = true,
+                AvailableFrom = new DateTime(2020, 05, 01),
+                AvailableTil = new DateTime(2020, 05, 02),
+                EventDate = new DateTime(2020, 05, 02),
+                EventText = "Volltext",
+                Length = new TimeSpan(0, 0, 60, 0),
+                LongTimeAvailable = true,
+                Organisation = new Organiser()
+                {
+                    OrganiserName = "Schauspielhaus Berlin"
+                },
+                StreamingLink = "https://www.google.de",
+                EventName = "Tanz der Vampire",
+                Thumbnail = "Thumbnail",
+                Thumbnail_Copyright = "(c) by ..."
+            };
+            _eventRepository.AddEvent(event1);
+        }
+
+        [HttpGet("createIndex")]
+        public void CreateIndex()
+        {
+            _eventRepository.CreateIndex();
+        }
+    }
+}
---
+++ Theateranmeldung.Web/Theateranmeldung.Web/Services/IEventRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Theateranmeldung.Web.Models.Data;
+
+namespace Theateranmeldung.Web.Services
+{
+    public interface IEventRepository
+    {
+        List<Event> GetEvents();
+
+        List<Event> GetEventById(string eventId);
+
+        void AddEvent(Event eventData);
+        List<Event> GetEventsFiltered(string wanted, string notwanted);
+        void CreateIndex();
+    }
+}
